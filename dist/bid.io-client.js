946d39eed0f0079462c2c3493891f179f9286023
Jonathan Brumley <cayasso@gmail.com>
2013-03-13 21:13:28 -0600

    Adding event file and updating channel

--- dist/bid.io-client.js
+++ dist/bid.io-client.js
@@ -124,6 +124,7 @@ if (window.localStorage) debug.enable(localStorage.debug);function require(p, pa
  * Module dependencies.
  */
 
+var Emitter = require('./events').EventEmitter;
 var debug = require('debug')('bid.io-client:channel');
 var parser = require('./parser');
 var encode = parser.encode;
@@ -175,6 +176,12 @@ function Channel (name, manager, fn) {
 }
 
 /**
+ * Inherits from `EventEmitter`.
+ */
+
+Channel.prototype.__proto__ = Emitter.prototype;
+
+/**
  * Get a `socket` instance and connect to it.
  *
  * @return {Channel} self
@@ -183,26 +190,26 @@ function Channel (name, manager, fn) {
 
 Channel.prototype.connect = function () {
   this.socket = this.io.connect(this.url + '/' + this.name, this.opts);
-  this.bind();
+  for (var i = 0, e; e = this.events[i]; i++) {
+    this.bind(e);
+  }
   return this;
 };
 
 /**
  * Bind socket events to channel.
  *
+ * @param {String} ev event name
  * @return {Channel} self
  * @api public
  */
 
-Channel.prototype.bind = function () {
+Channel.prototype.bind = function (ev) {
   var self = this;
-  for (var i = 0, e; e = this.events[i], i++) {
-    this.socket.on(e, function () {
-      var args = Array.prototype.slice.call(arguments);
-      self.emit.apply(self, [e].concat(args));
-    });
-  }
-  return this;
+  self.socket.on(ev, function () {
+    var args = Array.prototype.slice.call(arguments);
+    self.emit.apply(self, [ev].concat(args));
+  });
 };
 
 /**
@@ -364,6 +371,191 @@ Channel.prototype.send = function (type, id, owner, fn) {
     }
   });
 };
+});require.register("events.js", function(module, exports, require, global){
+/**
+ * Expose constructor.
+ */
+
+exports.EventEmitter = EventEmitter;
+
+/**
+ * Event emitter constructor.
+ *
+ * @api public.
+ */
+
+function EventEmitter () {};
+
+/**
+ * Adds a listener
+ *
+ * @api public
+ */
+
+EventEmitter.prototype.on = function (name, fn) {
+  if (!this.$events) {
+    this.$events = {};
+  }
+
+  if (!this.$events[name]) {
+    this.$events[name] = fn;
+  } else if (isArray(this.$events[name])) {
+    this.$events[name].push(fn);
+  } else {
+    this.$events[name] = [this.$events[name], fn];
+  }
+
+  return this;
+};
+
+EventEmitter.prototype.addListener = EventEmitter.prototype.on;
+
+/**
+ * Adds a volatile listener.
+ *
+ * @api public
+ */
+
+EventEmitter.prototype.once = function (name, fn) {
+  var self = this;
+
+  function on () {
+    self.removeListener(name, on);
+    fn.apply(this, arguments);
+  }
+
+  on.listener = fn;
+  this.on(name, on);
+
+  return this;
+};
+
+/**
+ * Removes a listener.
+ *
+ * @api public
+ */
+
+EventEmitter.prototype.removeListener = function (name, fn) {
+  if (this.$events && this.$events[name]) {
+    var list = this.$events[name];
+
+    if (isArray(list)) {
+      var pos = -1;
+
+      for (var i = 0, l = list.length; i < l; i++) {
+        if (list[i] === fn || (list[i].listener && list[i].listener === fn)) {
+          pos = i;
+          break;
+        }
+      }
+
+      if (pos < 0) {
+        return this;
+      }
+
+      list.splice(pos, 1);
+
+      if (!list.length) {
+        delete this.$events[name];
+      }
+    } else if (list === fn || (list.listener && list.listener === fn)) {
+      delete this.$events[name];
+    }
+  }
+
+  return this;
+};
+
+/**
+ * Removes all listeners for an event.
+ *
+ * @api public
+ */
+
+EventEmitter.prototype.removeAllListeners = function (name) {
+  if (name === undefined) {
+    this.$events = {};
+    return this;
+  }
+
+  if (this.$events && this.$events[name]) {
+    this.$events[name] = null;
+  }
+
+  return this;
+};
+
+/**
+ * Gets all listeners for a certain event.
+ *
+ * @api publci
+ */
+
+EventEmitter.prototype.listeners = function (name) {
+  if (!this.$events) {
+    this.$events = {};
+  }
+
+  if (!this.$events[name]) {
+    this.$events[name] = [];
+  }
+
+  if (!isArray(this.$events[name])) {
+    this.$events[name] = [this.$events[name]];
+  }
+
+  return this.$events[name];
+};
+
+/**
+ * Emits an event.
+ *
+ * @api public
+ */
+
+EventEmitter.prototype.emit = function (name) {
+  if (!this.$events) {
+    return false;
+  }
+
+  var handler = this.$events[name];
+
+  if (!handler) {
+    return false;
+  }
+
+  var args = Array.prototype.slice.call(arguments, 1);
+
+  if ('function' == typeof handler) {
+    handler.apply(this, args);
+  } else if (isArray(handler)) {
+    var listeners = handler.slice();
+
+    for (var i = 0, l = listeners.length; i < l; i++) {
+      listeners[i].apply(this, args);
+    }
+  } else {
+    return false;
+  }
+
+  return true;
+};
+
+/**
+ * Checks if the given object is an Array.
+ *
+ *     isArray([]); // true
+ *     isArray({}); // false
+ *
+ * @param Object obj
+ * @api public
+ */
+
+function isArray (obj) {
+  return '[object Array]' === Object.prototype.toString.call(obj);
+}
+
 });require.register("index.js", function(module, exports, require, global){
 
 /**
