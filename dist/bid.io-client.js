f1c2936a195d29eff0bf50a2b1ab91eff4c36e46
Jonathan Brumley <cayasso@gmail.com>
2013-03-29 13:04:03 -0600

    adding build

--- dist/bid.io-client.js
+++ dist/bid.io-client.js
@@ -976,6 +976,31 @@ function keys (obj){
   return arr;
 }
 });require.register("url.js", function(module, exports, require, global){
+/**
+ * Module exports
+ */
 
+exports.parse = parse;
+
+/**
+ * Parses an URI
+ *
+ * @author Steven Levithan <stevenlevithan.com> (MIT license)
+ * @api public
+ */
+
+var re = /^(?:(?![^:@]+:[^:@\/]*@)([^:\/?#.]+):)?(?:\/\/)?((?:(([^:@]*)(?::([^:@]*))?)?@)?([^:\/?#]*)(?::(\d*))?)(((\/(?:[^?#](?![^?#\/]*\.[^?#\/.]+(?:[?#]|$)))*\/?)?([^?#\/]*))(?:\?([^#]*))?(?:#(.*))?)/;
+var parts = ['source', 'protocol', 'authority', 'userInfo', 'user', 'password',
+             'host', 'port', 'relative', 'path', 'directory', 'file', 'query',
+             'anchor'];
+
+function parse (str) {
+  var m = re.exec(str || '') , uri = {} , i = 14;
+  while (i--) {
+    uri[parts[i]] = m[i] || '';
+  }
+ 
+  return uri;
+}
 });var exp = require('index.js');if ("undefined" != typeof module) module.exports = exp;else bio = exp;
 })();
